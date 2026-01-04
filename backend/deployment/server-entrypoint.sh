0ec6651a2eaa023013a59c1a401b2258cc2532e1
islam0122 <duishobaevislam01@gmail.com>
2026-01-04 23:57:28 +0300

    0---11

--- backend/deployment/server-entrypoint.sh
+++ backend/deployment/server-entrypoint.sh
@@ -1,33 +1,4 @@
 #!/bin/sh
 set -e
 
-echo "=== Starting entrypoint ==="
-
-# 1️⃣ Сбор статики
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-# 2️⃣ Проверка базы и миграции с ограничением попыток
-if [ "$RUN_MIGRATIONS" = "True" ]; then
-    echo "Running migrations..."
-    ATTEMPTS=0
-    MAX_ATTEMPTS=10
-    until python manage.py migrate || [ $ATTEMPTS -ge $MAX_ATTEMPTS ]; do
-        ATTEMPTS=$((ATTEMPTS+1))
-        echo "Waiting for DB to be ready... ($ATTEMPTS/$MAX_ATTEMPTS)"
-        sleep 3
-    done
-
-    if [ $ATTEMPTS -ge $MAX_ATTEMPTS ]; then
-        echo "⚠️ Database not ready after $MAX_ATTEMPTS attempts, exiting."
-        exit 1
-    fi
-fi
-
-# 3️⃣ Запуск сервера
-echo "Starting Gunicorn..."
-exec gunicorn config.wsgi:application \
-    --bind 0.0.0.0:${PORT:-8000} \
-    --log-level info \
-    --forwarded-allow-ips "*" \
-    --workers 3
+exec  python manage.py collectstatic --noinput && python manage.py makemigrations && python manage.py migrate && python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')" && gunicorn config.wsgi
