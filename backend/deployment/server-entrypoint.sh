58900796dd56304fde4f65681fd57a740fe31dfa
islam0122 <duishobaevislam01@gmail.com>
2026-01-04 23:46:18 +0300

    0---9

--- backend/deployment/server-entrypoint.sh
+++ backend/deployment/server-entrypoint.sh
@@ -1,8 +1,20 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/sh
 
-python manage.py migrate --noinput
+echo "Collecting static files.."
 python manage.py collectstatic --noinput
 
-exec gunicorn config.wsgi:application \
-  --bind 0.0.0.0:${PORT:-8000}
+if [ "$RUN_MIGRATIONS" = "True" ]; then
+    echo "Running migrations..."
+    until python manage.py migrate
+    do
+        echo "Waiting for db to be ready..."
+        sleep 2
+    done
+fi
+
+echo "Starting app server..."
+python -m gunicorn railway_django_stack.wsgi:application \
+    --bind 0.0.0.0:8000 \
+    --log-level info \
+    --config python:deployment.gunicorn_config \
+    --forwarded-allow-ips "*"
