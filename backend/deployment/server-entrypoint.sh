500cf78da41aceea5354333c0a32fc052dd9a985
islam0122 <duishobaevislam01@gmail.com>
2026-01-05 02:00:28 +0300

    0---23

--- backend/deployment/server-entrypoint.sh
+++ backend/deployment/server-entrypoint.sh
@@ -7,6 +7,8 @@ python manage.py migrate --noinput
 
 python manage.py shell -c "from django.contrib.auth.models import User; User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'admin')"
 
+python manage.py create_test_users
+
 exec gunicorn config.wsgi:application \
     --bind 0.0.0.0:${PORT:-8000} \
     --workers 3 \
