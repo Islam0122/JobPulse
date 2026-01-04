6ec6508ef33bbc30ab2b7877852097d002c4226f
islam0122 <duishobaevislam01@gmail.com>
2026-01-04 23:17:52 +0300

    0---4

--- backend/deployment/worker-entrypoint.sh
+++ backend/deployment/worker-entrypoint.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-celery -A config worker -l info --without-gossip --without-mingle --without-heartbeat
+
+celery -A config worker -l info \
+  --concurrency=2 \
+  --without-gossip \
+  --without-mingle \
+  --without-heartbeat
