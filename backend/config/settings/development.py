591ffa12c96dd9aced8182ae7e4ee19e7dc8c663
islam0122 <duishobaevislam01@gmail.com>
2026-01-05 01:47:48 +0300

    0---22

--- backend/config/settings/development.py
+++ backend/config/settings/development.py
@@ -17,9 +17,6 @@ DATABASES = {
     }
 }
 
-
-MIDDLEWARE.insert(0, 'debug_toolbar.middleware.DebugToolbarMiddleware')
-
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = env('EMAIL_HOST', default='smtp.gmail.com')
 EMAIL_PORT = env.int('EMAIL_PORT', default=587)
