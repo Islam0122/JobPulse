19c1cfd3062932efd88b143cfd1ef5ae01e3fe48
islam0122 <duishobaevislam01@gmail.com>
2026-01-13 15:52:39 +0300

    fix: Celery

--- backend/config/celery.py
+++ backend/config/celery.py
@@ -1,10 +1,13 @@
 import os
 from celery import Celery
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
 app = Celery('jobpulse')
 app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.enable_utc = True
+app.conf.timezone = settings.TIME_ZONE
 app.autodiscover_tasks()
 
 @app.task(bind=True, ignore_result=True)
